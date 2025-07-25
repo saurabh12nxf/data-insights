dc4cb72ee62adf6b04dd39ede18413b9eb447abd
Saurabh Singh <sureshsingh09120@gmail.com>
2025-07-25 20:18:52 +0530

    Add STATIC_ROOT for collectstatic in Render deploy

--- data_insights_platform/settings.py
+++ data_insights_platform/settings.py
@@ -53,3 +53,5 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'django.contrib.staticfiles.storage.StaticFilesStorage'
