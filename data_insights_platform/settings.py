bcc526928a5fc21604818ef01c941c0c8f5966ae
Saurabh Singh <sureshsingh09120@gmail.com>
2025-07-25 16:54:33 +0530

    Updated settings.py for Render deploy

--- data_insights_platform/settings.py
+++ data_insights_platform/settings.py
@@ -1,8 +1,10 @@
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'your-secret-key'
-DEBUG = True
-ALLOWED_HOSTS = []
+DEBUG = False
+ALLOWED_HOSTS = [os.environ.get('RENDER_EXTERNAL_HOSTNAME', 'localhost')]
+
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
